93a6debb12b8127d86ca20a9b862fd9590842e9b
Dave Bage <david_bage@hotmail.com>
2022-09-30 14:33:30 +0100

    Create new Coordinate and Move classes

---
+++ Tic_Tac_Toe_OC_Kata/Coordinate.cs
@@ -0,0 +1,14 @@
+﻿namespace Tic_Tac_Toe_OC_Kata;
+
+public class Coordinate
+{
+    private readonly int _column;
+
+    private readonly int _row;
+
+    public Coordinate(int column, int row)
+    {
+        _column = column;
+        _row = row;
+    }
+}
---
+++ Tic_Tac_Toe_OC_Kata/Move.cs
@@ -0,0 +1,25 @@
+﻿namespace Tic_Tac_Toe_OC_Kata;
+
+public class Move
+{
+    private readonly BoardToken _boardToken;
+
+    private readonly Coordinate _coordinate;
+
+    public Move(BoardToken boardToken, Coordinate coordinate)
+    {
+        _boardToken = boardToken;
+        _coordinate = coordinate;
+
+    }
+
+    public bool CompareToken(BoardToken other)
+    {
+        return other == _boardToken;
+    }
+
+    public BoardToken GetToken()
+    {
+        return _boardToken;
+    }
+}
