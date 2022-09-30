fa24f83d5a47458662f6bfa4304ecf94e7df157a
Dave Bage <david_bage@hotmail.com>
2022-09-30 14:34:15 +0100

    Move Coordinate and Move classes into their own files

--- Tic_Tac_Toe_OC_Kata/TicTacToe.cs
+++ Tic_Tac_Toe_OC_Kata/TicTacToe.cs
@@ -1,33 +1,19 @@
 ﻿namespace Tic_Tac_Toe_OC_Kata;
 
-public class Coordinate
-{
-    public int Column;
-
-    public int Row;
-}
-
-public class Move
-{
-    public BoardToken boardToken;
-
-    public Coordinate coordinate;
-}
-
 public class TicTacToe
 {
-    private BoardToken? _lastPlacedToken;
+    private BoardToken _lastPlacedToken = BoardToken.O;
 
     public PlaceTokenResult PlaceToken(Move move)
     {
         var placeTokenResult = new PlaceTokenResult();
 
-        if (!_lastPlacedToken.HasValue && move.boardToken == BoardToken.O || move.boardToken == _lastPlacedToken)
+        if (move.CompareToken(_lastPlacedToken))
             placeTokenResult.Successful = false;
         
         if (!placeTokenResult.Successful) return placeTokenResult;
 
-        _lastPlacedToken = move.boardToken;
+        _lastPlacedToken = move.GetToken();
 
         return placeTokenResult;
     }
