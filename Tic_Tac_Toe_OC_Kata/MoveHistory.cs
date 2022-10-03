d2209892b065c8bd22a8c15d39365607fdd1c0a2
Dave Bage <david_bage@hotmail.com>
2022-10-03 20:34:19 +0100

    Place MoveHistory into own file, create new GameWonStatus

---
+++ Tic_Tac_Toe_OC_Kata/MoveHistory.cs
@@ -0,0 +1,40 @@
+﻿namespace Tic_Tac_Toe_OC_Kata;
+
+public class MoveHistory
+{
+    private readonly List<Move> _moves = new List<Move>();
+
+    public PlaceTokenResult AddMove(Move move)
+    {
+        if (!_moves.Any() && move.CompareToken(BoardToken.O))
+            return PlaceTokenResult.Failure;
+
+        if (_moves.Any() && move.CompareToken(_moves.Last()))
+            return PlaceTokenResult.Failure;
+
+        if (_moves.Any(x => x.CompareCoordinates(move)))
+            return PlaceTokenResult.Failure;
+
+        _moves.Add(move);
+
+        if (IsHorizontalWin(move) == GameWonStatus.GameWon) return PlaceTokenResult.GameWon;
+
+        return PlaceTokenResult.Success;
+
+    }
+
+    private GameWonStatus IsHorizontalWin(Move move)
+    {
+        BoardToken token = move.CompareToken(BoardToken.O) ? BoardToken.O : BoardToken.X;
+
+        for (int rowIndex = 0; rowIndex < 3; rowIndex++)
+        {
+            if(_moves.Any(x => x.Equals(new Move(token, new Coordinate(0, rowIndex)))) &&
+                    _moves.Any(x => x.Equals(new Move(token, new Coordinate(1, rowIndex)))) &&
+                    _moves.Any(x => x.Equals(new Move(token, new Coordinate(2, rowIndex)))))
+                return GameWonStatus.GameWon;
+        }
+
+        return GameWonStatus.GameNotWon;
+    }
+}
--- Tic_Tac_Toe_OC_Kata/TicTacToe.cs
+++ Tic_Tac_Toe_OC_Kata/TicTacToe.cs
@@ -1,46 +1,5 @@
 ﻿namespace Tic_Tac_Toe_OC_Kata;
 
-public class MoveHistory
-{
-    private readonly List<Move> _moves = new List<Move>();
-
-    public PlaceTokenResult AddMove(Move move)
-    {
-        if (!_moves.Any() && move.CompareToken(BoardToken.O))
-            return PlaceTokenResult.Failure;
-
-        if (_moves.Any() && move.CompareToken(_moves.Last()))
-            return PlaceTokenResult.Failure;
-
-        if (_moves.Any(x => x.CompareCoordinates(move)))
-            return PlaceTokenResult.Failure;
-
-        _moves.Add(move);
-
-        for (int rowIndex = 0; rowIndex < 3; rowIndex++)
-        {
-            if (_moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(0, rowIndex)))) &&
-                _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(1, rowIndex)))) &&
-                _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(2, rowIndex)))))
-                return PlaceTokenResult.GameWon;
-        }
-
-        for (int rowIndex = 0; rowIndex < 3; rowIndex++)
-        {
-            if (_moves.Any(x => x.Equals(new Move(BoardToken.O, new Coordinate(0, rowIndex)))) &&
-                _moves.Any(x => x.Equals(new Move(BoardToken.O, new Coordinate(1, rowIndex)))) &&
-                _moves.Any(x => x.Equals(new Move(BoardToken.O, new Coordinate(2, rowIndex)))))
-                return PlaceTokenResult.GameWon;
-        }
-
-        return PlaceTokenResult.Success;
-
-    }
-
-
-}
-
-
 public class TicTacToe
 {
     private readonly MoveHistory _moveHistory = new MoveHistory();
