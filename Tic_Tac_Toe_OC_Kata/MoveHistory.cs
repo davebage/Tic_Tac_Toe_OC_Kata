d25ec27cfd4de9787c79b4514a7b4ac8c2663ff4
Dave Bage <david_bage@hotmail.com>
2022-10-03 21:52:19 +0100

    refactored win check functions

--- Tic_Tac_Toe_OC_Kata/MoveHistory.cs
+++ Tic_Tac_Toe_OC_Kata/MoveHistory.cs
@@ -17,33 +17,41 @@ public class MoveHistory
 
         _moves.Add(move);
 
-        if (IsHorizontalWin(move) == GameWonStatus.GameWon) return PlaceTokenResult.GameWon;
-        if (IsVerticalWin(move) == GameWonStatus.GameWon) return PlaceTokenResult.GameWon;
-
-        if (_moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(0, 0)))) &&
-            _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(1, 1)))) &&
-            _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(2, 2)))))
-            return PlaceTokenResult.GameWon;
-
-        if (_moves.Any(x => x.Equals(new Move(BoardToken.O, new Coordinate(0, 0)))) &&
-            _moves.Any(x => x.Equals(new Move(BoardToken.O, new Coordinate(1, 1)))) &&
-            _moves.Any(x => x.Equals(new Move(BoardToken.O, new Coordinate(2, 2)))))
-            return PlaceTokenResult.GameWon;
-
-        if (_moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(0, 2)))) &&
-            _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(1, 1)))) &&
-            _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(2, 0)))))
-            return PlaceTokenResult.GameWon;
+        if(HasPlacedTokenWon(move) == GameWonStatus.GameWon) return PlaceTokenResult.GameWon;
 
 
         return PlaceTokenResult.Success;
 
     }
 
-    private GameWonStatus IsHorizontalWin(Move move)
+    private GameWonStatus HasPlacedTokenWon(Move move)
     {
         BoardToken token = move.CompareToken(BoardToken.O) ? BoardToken.O : BoardToken.X;
 
+        if (IsHorizontalWin(token, move) == GameWonStatus.GameWon) return IsHorizontalWin(token, move);
+        if (IsVerticalWin(token, move) == GameWonStatus.GameWon) return IsVerticalWin(token, move);
+        if (IsDiagonalWin(token, move) == GameWonStatus.GameWon) return IsDiagonalWin(token, move);
+
+        return GameWonStatus.GameNotWon;
+    }
+
+    private GameWonStatus IsDiagonalWin(BoardToken token, Move move)
+    {
+        if (_moves.Any(x => x.Equals(new Move(token, new Coordinate(0, 0)))) &&
+            _moves.Any(x => x.Equals(new Move(token, new Coordinate(1, 1)))) &&
+            _moves.Any(x => x.Equals(new Move(token, new Coordinate(2, 2)))))
+            return GameWonStatus.GameWon;
+
+        if (_moves.Any(x => x.Equals(new Move(token, new Coordinate(0, 2)))) &&
+            _moves.Any(x => x.Equals(new Move(token, new Coordinate(1, 1)))) &&
+            _moves.Any(x => x.Equals(new Move(token, new Coordinate(2, 0)))))
+            return GameWonStatus.GameWon;
+
+        return GameWonStatus.GameNotWon;
+    }
+
+    private GameWonStatus IsHorizontalWin(BoardToken token, Move move)
+    {
         for (int rowIndex = 0; rowIndex < 3; rowIndex++)
         {
             if(_moves.Any(x => x.Equals(new Move(token, new Coordinate(0, rowIndex)))) &&
@@ -55,10 +63,8 @@ public class MoveHistory
         return GameWonStatus.GameNotWon;
     }
 
-    private GameWonStatus IsVerticalWin(Move move)
+    private GameWonStatus IsVerticalWin(BoardToken token, Move move)
     {
-        BoardToken token = move.CompareToken(BoardToken.O) ? BoardToken.O : BoardToken.X;
-
         for (int columnIndex = 0; columnIndex < 3; columnIndex++)
         {
             if (_moves.Any(x => x.Equals(new Move(token, new Coordinate(columnIndex, 0)))) &&
