aa09125495f84eb8c50e8751fabfcee61793203f
Dave Bage <david_bage@hotmail.com>
2022-10-03 19:06:50 +0100

    Refactor to first class collections and move List<Move> into MoveHistory class Add horizontal win checks for O token

--- Tic_Tac_Toe_OC_Kata/TicTacToe.cs
+++ Tic_Tac_Toe_OC_Kata/TicTacToe.cs
@@ -1,10 +1,10 @@
 ﻿namespace Tic_Tac_Toe_OC_Kata;
 
-public class TicTacToe
+public class MoveHistory
 {
     private readonly List<Move> _moves = new List<Move>();
 
-    public PlaceTokenResult PlaceToken(Move move)
+    public PlaceTokenResult AddMove(Move move)
     {
         if (!_moves.Any() && move.CompareToken(BoardToken.O))
             return PlaceTokenResult.Failure;
@@ -17,21 +17,36 @@ public class TicTacToe
 
         _moves.Add(move);
 
-        if(_moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(0, 0)))) &&
-           _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(1, 0)))) &&
-           _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(2, 0)))))
-            return PlaceTokenResult.GameWon;
+        for (int rowIndex = 0; rowIndex < 3; rowIndex++)
+        {
+            if (_moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(0, rowIndex)))) &&
+                _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(1, rowIndex)))) &&
+                _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(2, rowIndex)))))
+                return PlaceTokenResult.GameWon;
+        }
 
-        if (_moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(0, 1)))) &&
-            _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(1, 1)))) &&
-            _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(2, 1)))))
-            return PlaceTokenResult.GameWon;
-
-        if (_moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(0, 2)))) &&
-            _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(1, 2)))) &&
-            _moves.Any(x => x.Equals(new Move(BoardToken.X, new Coordinate(2, 2)))))
-            return PlaceTokenResult.GameWon;
+        for (int rowIndex = 0; rowIndex < 3; rowIndex++)
+        {
+            if (_moves.Any(x => x.Equals(new Move(BoardToken.O, new Coordinate(0, rowIndex)))) &&
+                _moves.Any(x => x.Equals(new Move(BoardToken.O, new Coordinate(1, rowIndex)))) &&
+                _moves.Any(x => x.Equals(new Move(BoardToken.O, new Coordinate(2, rowIndex)))))
+                return PlaceTokenResult.GameWon;
+        }
 
         return PlaceTokenResult.Success;
+
+    }
+
+
+}
+
+
+public class TicTacToe
+{
+    private readonly MoveHistory _moveHistory = new MoveHistory();
+
+    public PlaceTokenResult PlaceToken(Move move)
+    {
+        return _moveHistory.AddMove(move);
     }
 }
--- Tic_Tac_Toe_OC_Kata_Tests/Tic_Tac_Toe_Should.cs
+++ Tic_Tac_Toe_OC_Kata_Tests/Tic_Tac_Toe_Should.cs
@@ -100,6 +100,50 @@ namespace Tic_Tac_Toe_OC_Kata_Tests
             var result = _ticTacToe.PlaceToken(new Move(BoardToken.X, new Coordinate(2, 2)));
             Assert.That(result, Is.EqualTo(PlaceTokenResult.GameWon));
         }
+
+
+        [Test]
+        public void Check_O_Won_Along_Bottom_Row_After_Three_Plays()
+        {
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.X, new Coordinate(0, 1))), Is.EqualTo(PlaceTokenResult.Success));
+
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.O, new Coordinate(0, 0))), Is.EqualTo(PlaceTokenResult.Success));
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.X, new Coordinate(1, 2))), Is.EqualTo(PlaceTokenResult.Success));
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.O, new Coordinate(1, 0))), Is.EqualTo(PlaceTokenResult.Success));
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.X, new Coordinate(0, 3))), Is.EqualTo(PlaceTokenResult.Success));
+            var result = _ticTacToe.PlaceToken(new Move(BoardToken.O, new Coordinate(2, 0)));
+            Assert.That(result, Is.EqualTo(PlaceTokenResult.GameWon));
+        }
+
+        [Test]
+        public void Check_O_Won_Along_Middle_Row_After_Three_Plays()
+        {
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.X, new Coordinate(0, 0))), Is.EqualTo(PlaceTokenResult.Success));
+
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.O, new Coordinate(0, 1))), Is.EqualTo(PlaceTokenResult.Success));
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.X, new Coordinate(1, 2))), Is.EqualTo(PlaceTokenResult.Success));
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.O, new Coordinate(1, 1))), Is.EqualTo(PlaceTokenResult.Success));
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.X, new Coordinate(0, 3))), Is.EqualTo(PlaceTokenResult.Success));
+            var result = _ticTacToe.PlaceToken(new Move(BoardToken.O, new Coordinate(2, 1)));
+            Assert.That(result, Is.EqualTo(PlaceTokenResult.GameWon));
+        }
+
+        [Test]
+        public void Check_O_Won_Along_Top_Row_After_Three_Plays()
+        {
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.X, new Coordinate(0, 0))), Is.EqualTo(PlaceTokenResult.Success));
+
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.O, new Coordinate(0, 2))), Is.EqualTo(PlaceTokenResult.Success));
+
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.X, new Coordinate(1, 1))), Is.EqualTo(PlaceTokenResult.Success));
+
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.O, new Coordinate(1, 2))), Is.EqualTo(PlaceTokenResult.Success));
+
+            Assert.That(_ticTacToe.PlaceToken(new Move(BoardToken.X, new Coordinate(0, 1))), Is.EqualTo(PlaceTokenResult.Success));
+            var result = _ticTacToe.PlaceToken(new Move(BoardToken.O, new Coordinate(2, 2)));
+            Assert.That(result, Is.EqualTo(PlaceTokenResult.GameWon));
+        }
+
     }
 
 
